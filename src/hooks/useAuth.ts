98974b9629ff74fae90b82e0335b5a177ebc1924
Borngreat Ikwutah <iagocech2056@gmail.com>
2024-12-31 17:46:16 +0100

    Updated: Dashboard Features to Include Session Storage

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,21 +2,21 @@ import { useState } from "react";
 
 const useAuth = () => {
   const [isSignedIn, setIsSignedIn] = useState<boolean>(() => {
-    const token = localStorage.getItem("token-storage");
+    const token = sessionStorage.getItem("token-storage");
     return !!token;
   });
 
   const getToken = () => {
-    return localStorage.getItem("token-storage");
+    return sessionStorage.getItem("token-storage");
   };
 
   const signIn = (token: string) => {
-    localStorage.setItem("token-storage", token);
+    sessionStorage.setItem("token-storage", token);
     setIsSignedIn(true);
   };
 
   const signOut = () => {
-    localStorage.removeItem("token-storage");
+    sessionStorage.removeItem("token-storage");
     setIsSignedIn(false);
   };
 
